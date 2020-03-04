c7fa95ef9f7fa5260745566c31f5696acb8df5cd
ilyapishchulin <ilyapishchulin@mail.ru>
2020-03-04 11:13:43 +0300

    Minor fix

---
+++ precache-manifest.9144b3f92a8e3bfe4019ae05a782025c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237136395721f0acee127093ccdb5439",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c75adc919ec3c638ac79",
+    "url": "./static/css/2.70e03ba6.chunk.css"
+  },
+  {
+    "revision": "5c2f0b818d267b742bfd",
+    "url": "./static/css/main.96774782.chunk.css"
+  },
+  {
+    "revision": "c75adc919ec3c638ac79",
+    "url": "./static/js/2.c2a498f2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.c2a498f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2f0b818d267b742bfd",
+    "url": "./static/js/main.20f90281.chunk.js"
+  },
+  {
+    "revision": "66d33f9665461d752572",
+    "url": "./static/js/runtime-main.9635745e.js"
+  }
+]);
